4eca4a8a7e6c85d839bfcfa493f6f96b97a24498
Nagaraja Balakrishna <nbalakrishna@manh.com>
2020-06-26 15:17:53 +0530

    test class to see maven-surefire-plugin

---
+++ src/test/java/HelloWorldTest.java
@@ -0,0 +1,23 @@
+/*
+ * maven-surefile-plugin picks classes starting/ending with name Test and runs methods starting with test
+ * 
+ */
+public class HelloWorldTest {
+
+	public void testHello() {
+		System.out.println("Test - Hello...");
+	}
+
+	public void testWorld() {
+		System.out.println("Test - World...");
+	}
+
+	public void helloTest() {
+		System.out.println("Test - Hello...");
+	}
+
+	public void worldTest() {
+		System.out.println("Test - World...");
+	}
+
+}
